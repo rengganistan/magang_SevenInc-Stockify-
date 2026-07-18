035883577e8ac14d0a10e2a84092ddc64c259257
rengganis <rengganistantri4@gmail.com>
2026-07-18 14:12:58 +0700

    refactor: finalize User model authentication and role integration

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements CanResetPasswordContract
 {
-    use CanResetPassword;
+    use CanResetPassword, Notifiable;
 
     protected $fillable = [
         'name',
